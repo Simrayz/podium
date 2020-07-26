76ae18375c6a67e00a2b54b876710064968b6494
Simrayz <simenabelsen@hotmail.no>
2020-07-26 18:26:47 +0200

    Init commit

---
+++ components/Counter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mx-auto shadow-lg p-5">
+    <div class="text-gray-900">{{ counter }}</div>
+    <button @click="increment" class="border rounded text-gray-800">
+      Increment
+    </button>
+  </div>
+</template>
+<script>
+import { ref } from 'nuxt-composition-api'
+export default {
+  setup() {
+    const counter = ref(0)
+    const increment = () => {
+      counter.value++
+    }
+    return {
+      counter,
+      increment,
+    }
+  },
+}
+</script>
---
+++ pages/hello.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mx-auto bg-blue-300 min-h-screen">
+    <div class="text-2xl">Hello World!</div>
+    <counter />
+  </div>
+</template>
+<script>
+import Counter from '@/components/Counter'
+export default {
+  components: {
+    Counter,
+  },
+}
+</script>
